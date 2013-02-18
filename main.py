132c937207e9380d4775102b9ed722dc31206e0f
DivinityArcane <eittreim.justin@live.com>
2013-02-18 12:35:14 -0800

    Wat.

---
+++ main.py
@@ -0,0 +1,15 @@
+from ui import interface
+
+class Client:
+	def __init__(self):
+		self.ui = interface()
+		self.ui.start()
+		self.running = True
+		self.loop()
+
+	def loop(self):
+		while self.running:
+			self.ui.check_input()
+
+
+cli = Client()
---
+++ ui.py
@@ -0,0 +1,31 @@
+import urwid
+
+class interface:
+	def __init__(self):
+		self.window = urwid.Padding(self.config_menu(), left=3, right=3)
+		self.shade = urwid.Overlay(self.window, urwid.SolidFill('\N{MEDIUM SHADE}'), align='center', width=('relative', 50), valign='middle', height=('relative', 50), min_width=50, min_height=10)
+
+	def config_menu(self):
+		content = [urwid.Text('Config'), urwid.Divider()]
+
+		for opt in ['Username', 'Password', 'Channels', 'Etc']:
+			button = urwid.Button(opt)
+			urwid.connect_signal(button, 'click', self.on_config_click, opt)
+			content.append(urwid.AttrMap(button, None))
+
+		return urwid.ListBox(urwid.SimpleFocusListWalker(content))
+
+	def on_config_click(self, button, option):
+		resp = urwid.Text(['You clicked ', option, '!\n'])
+		exit = urwid.Button('Exit')
+		urwid.connect_signal(exit, 'click', self.close)
+		self.window.original_widget = urwid.Filler(urwid.Pile([resp, urwid.AttrMap(exit, None)]))
+
+	def start(self):
+		urwid.MainLoop(self.shade, palette=[('reversed', 'standout', '')]).run()
+
+	def check_input(self):
+		pass
+		
+	def close(self):
+		raise urwid.ExitMainLoop()
